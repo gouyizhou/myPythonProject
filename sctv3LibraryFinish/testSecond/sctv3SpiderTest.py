d281a2e474d355c57c4d9200ef5c5cdd5e5ce69d
gouyizhou <65145246+gouyizhou@users.noreply.github.com>
2021-05-15 16:35:30 +0800

    Add files via upload

---
+++ sctv3LibraryFinish/testSecond/sctv3SpiderTest.py
@@ -0,0 +1,98 @@
+# !/usr/bin/env python
+# -*-coding:utf-8-*-
+# date :2021/5/2 17:19
+# author:Sabo
+import requests
+from bs4 import BeautifulSoup
+import os
+
+savePath = 'F:/maLaTangTest'
+
+# 通过首页获取每个视频的Url
+# 通过单个视频的Url利用you-get进行下载相应的视频
+
+# 网站头加上各个分视频的网址即可得到单个视频的网址，从而即可利用you-get进行下载
+signalUrlHead = 'http://show.sctv.com/mlt/'
+homePageUrl = 'http://show.sctv.com/mlt/index.shtml'
+def getUrls(homePageUrl):
+    results = []
+    response = requests.get(url = homePageUrl)
+    if response.status_code != 200:
+        print('Get urls error!')
+        return []
+    else:
+        print('Get urls successfully!')
+        response.encoding = 'utf-8'
+        txt = response.text
+        homePage = BeautifulSoup(txt, 'html.parser')
+        childUrls = homePage.find_all('div', attrs={"class":"txt"})
+        for index in range(0, childUrls.__len__()):
+            link = childUrls[index].find('a').get('href')
+            results.append(link)
+        return results
+
+def catUrl(signalHead, signalLinks):
+    results = []
+    for index in range(0, signalLinks.__len__()):
+        results.append(signalUrlHead+signalLinks[index])
+    return results
+
+def download(savePath, vedioName, vedioUrl):
+    commond = 'you-get -o {0} -O {1} "{2}"'.format(savePath, vedioName, vedioUrl)
+    print(commond)
+    os.system(commond)
+
+def getTitles(homePage):
+    titles = []
+    response = requests.get(url = homePage)
+    if response.status_code != 200:
+        print('Get titles error!')
+        return ''
+    else:
+        print("Get titles successfully!")
+        response.encoding = 'utf-8'
+        txt = response.text
+        mainPage = BeautifulSoup(txt, 'html.parser')
+        childTitles = mainPage.find_all('div', attrs={'class':'name'})
+        for index in range(0, childTitles.__len__()):
+            print(childTitles[index].text)
+            titles.append(childTitles[index].text.replace(' ', '-') )
+        return titles
+
+
+# 根据域名格式合成所有的域名
+# 首页  http://show.sctv.com/mlt/index.shtml
+# 第二页 http://show.sctv.com/mlt/index_1.shtml
+# 第三页 http://show.sctv.com/mlt/index_2.shtml
+# 格式为 http://show.sctv.com/mlt/index_x.shtml
+
+def getAllHomePage(homePageUrl):
+    catFlag = 1
+    resultHomePageUrl = []
+    resultHomePageUrl.append(homePageUrl)
+    while(catFlag < 10):
+        resultHomePageUrl.append(homePageUrl.replace('.shtml', '_'+catFlag.__str__()+'.shtml'))
+        catFlag+=1
+    return resultHomePageUrl
+
+if __name__ == '__main__':
+    homePages = getAllHomePage(homePageUrl  = homePageUrl)
+    for index in range(1, homePages.__len__()):
+        # print("index:",index)
+        # print('Now website is %s' %(homePages[index]))
+        # 当前主页面的所有视频标题
+        titles = getTitles(homePage=homePages[index])
+        print(titles)
+        # 当前页面下的所有视频独立路径
+        signalLinks = getUrls(homePageUrl=homePages[index])
+        # 独立路径和主路径完成拼接，得到完整的视频路径
+        completeUrls = catUrl(signalHead = signalUrlHead, signalLinks = signalLinks)
+        # for i in range(0, completeUrls.__len__()):
+        #     print("titles:", titles[i])
+        #     print("signal links:",signalLinks[i])
+
+        # # 循环下载
+        for indexDownLoad in range(0, completeUrls.__len__()):
+            download(savePath=savePath, vedioName=titles[indexDownLoad], vedioUrl=completeUrls[indexDownLoad])
+    # homePages = getAllHomePage(homePageUrl=homePageUrl)
+
---
+++ sctv3LibraryFinish/testSecond/test1.py
@@ -0,0 +1,6 @@
+# !/usr/bin/env python
+# -*-coding:utf-8-*-
+# date :2021/5/2 18:04
+# author:Sabo
+str= '麻辣烫：欢喜嗨店 第14集'.replace(' ','-')
+print(str)
